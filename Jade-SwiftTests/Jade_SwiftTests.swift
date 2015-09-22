5cad23abdca7b813f298944545d96263f5d81720
CleÃÅment Roulland <clement.roulland@gmail.com>
2015-09-22 20:40:48 +0200

    Initial Commit

---
+++ Jade-SwiftTests/Jade_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  Jade_SwiftTests.swift
+//  Jade-SwiftTests
+//
+//  Created by Clément Roulland on 22/09/2015.
+//  Copyright © 2015 Brozh. All rights reserved.
+//
+
+import XCTest
+@testable import Jade_Swift
+
+class Jade_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
